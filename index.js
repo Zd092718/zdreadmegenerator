ea7a3127609cbef0c78cc5262b9618ff2751f295
Zachary Dowd <zdowd2796@gmail.com>
2021-08-25 07:24:28 -0600

    questions added

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const md = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [  
 {
     type: 'input',
-    name: 'first_name',
-    message: "What's your first name",
+    name: 'project_title',
+    message: "What's the name of your project?",
 },
 {
     type: 'input',
-    name: 'last_name',
-    message: "What's your last name",
-    default() {
-      return 'Doe';
-    },
+    name: 'username',
+    message: "What's your Github username?",
+},
+{
+    type: 'input',
+    name: 'description',
+    message: "Describe your project",
+},
+{
+    type: 'input',
+    name: 'usage_information',
+    message: "How is your project used?",
+},
+{
+    type: 'input',
+    name: 'contriubution_guidelines',
+    message: "Who contributed to your project?",
+},
+{
+    type: 'input',
+    name: 'test_instructions',
+    message: "How to test your program",
+},
+{
+    type: 'input',
+    name: 'email_address',
+    message: "What's your email address?",
 },
 {
     type: 'list',
     name: 'license',
-    message: 'What size do you need?',
-    choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+    message: 'What license is used for your application?',
+    choices: ['MIT', 'Apache', 'Mozilla', 'Common DevelopmenT and Distribution', 'Eclipse', 'GNU'],
     filter(val) {
       return val.toLowerCase();
     },
 }
 ];
 
+// inquirer.prompt(questions).then((answers) => {
+//     console.log(JSON.stringify(answers, null, ' '))
+// })
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, md.generateMarkdown(data), )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(writeToFile('readme.md', data))
+}
 
 // Function call to initialize app
 init();
