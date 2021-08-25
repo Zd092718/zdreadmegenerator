5cb5eb46cd93d52194fb0feac6bddda9529e4bdb
Zachary Dowd <zdowd2796@gmail.com>
2021-08-25 09:19:28 -0600

    license functions working

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const md = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [  
 {
     type: 'input',
-    name: 'project_title',
+    name: 'projectTitle',
     message: "What's the name of your project?",
 },
 {
@@ -17,33 +17,38 @@ const questions = [
 {
     type: 'input',
     name: 'description',
-    message: "Describe your project",
+    message: "Describe your project:",
 },
 {
     type: 'input',
-    name: 'usage_information',
-    message: "How is your project used?",
+    name: 'languages',
+    message: "What languages did your project use?",
 },
 {
     type: 'input',
-    name: 'contriubution_guidelines',
+    name: 'usage',
+    message: "How will your project be used?",
+},
+{
+    type: 'input',
+    name: 'contributions',
     message: "Who contributed to your project?",
 },
 {
     type: 'input',
-    name: 'test_instructions',
-    message: "How to test your program",
+    name: 'testInstructions',
+    message: "How to test your program:",
 },
 {
     type: 'input',
-    name: 'email_address',
+    name: 'emailAddress',
     message: "What's your email address?",
 },
 {
     type: 'list',
     name: 'license',
     message: 'What license is used for your application?',
-    choices: ['MIT', 'Apache', 'Mozilla', 'Common DevelopmenT and Distribution', 'Eclipse', 'GNU'],
+    choices: ['MIT', 'Apache', 'Mozilla', 'Boost', 'Eclipse', 'SIL'],
     filter(val) {
       return val.toLowerCase();
     },
@@ -55,12 +60,15 @@ const questions = [
 // })
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, md.generateMarkdown(data), )
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Success!') )
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(writeToFile('readme.md', data))
+    inquirer.prompt(questions)
+    .then((data) => generateMarkdown(data))
+    .then((data) => writeToFile('UREADME.md', data))
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,31 +1,108 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  license = license.toUpperCase();
   switch (license){
-    
+    case 'MIT':
+      return 'https://img.shields.io/badge/License-MIT-yellow.svg'
+    break;
+    case 'Mozilla':
+      return 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg'
+    break;
+    case 'Apache':
+      return 'https://img.shields.io/badge/License-Apache%202.0-blue.svg'
+    break;
+    case 'Boost':
+      return 'https://img.shields.io/badge/License-Boost%201.0-lightblue.svg'
+    break;
+    case 'Eclipse':
+      return 'https://img.shields.io/badge/License-EPL%201.0-red.svg'
+    break;
+    case 'SIL':
+      return 'https://img.shields.io/badge/License-OFL%201.1-lightgreen.svg'
+    break;
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  license = license.toUpperCase();
   switch (license){
-    
+    case 'MIT':
+        return 'https://opensource.org/licenses/MIT'
+    break;
+    case 'Mozilla':
+        return 'https://opensource.org/licenses/MPL-2.0'
+    break;
+    case 'Apache':
+        return 'https://opensource.org/licenses/Apache-2.0'
+    break;
+    case 'Boost':
+        return 'https://www.boost.org/LICENSE_1_0.txt'
+    break;
+    case 'Eclipse':
+        return 'https://opensource.org/licenses/EPL-1.0'
+    break;
+    case 'SIL':
+        return 'https://opensource.org/licenses/OFL-1.1'
+    break;
   }
+  console.log(license)
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  switch (license){
-
+  if(license){
+    return `[![License](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`
+  } else {
+    return ' '
   }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
+  return `# ${data.projectTitle}
+  <details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li><a href="#testing">Testing</a></li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+  </details>
+  <br>
+
+  ## About the Project
+  ${data.description}
+
+  ### Built With:
+  * ${data.languages}
+
+  ## Testing
+  ${data.testInstructions}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributions}
+
+  ## Contact
+  Github Profile: https://github.com/${data.username}
+  <br>
+  Email: <a href="mailto: ${data.emailAddress}">${data.emailAddress}</a>
+  <br>
+  ${renderLicenseSection(data.license)}
 `;
 }
 
